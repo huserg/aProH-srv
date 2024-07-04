a7dbeb0af82a895960bcada18cb4eca94378d8f4
Huser Gaétan <gaetan.huser@netplus.ch>
2024-07-04 09:51:35 +0200

    fix gray

--- resources/views/device/show.blade.php
+++ resources/views/device/show.blade.php
@@ -58,8 +58,8 @@
                                 <li class="py-4 flex justify-between items-center">
                                     <div class="flex items-center">
                                         <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">{{ $relatedDevice->user->name }}</span>
-                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-{{ $relatedDevice->status ?? 'grey' }}-100 text-{{ $relatedDevice->status ?? 'grey' }}-800 dark:bg-{{ $relatedDevice->status ?? 'grey' }}-800 dark:text-{{ $relatedDevice->status ?? 'grey' }}-100">
-                                            {{ $relatedDevice->status ?? 'grey' }}
+                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-{{ $relatedDevice->status ?? 'gray' }}-100 text-{{ $relatedDevice->status ?? 'gray' }}-800 dark:bg-{{ $relatedDevice->status ?? 'gray' }}-800 dark:text-{{ $relatedDevice->status ?? 'gray' }}-100">
+                                            {{ $relatedDevice->status ?? 'gray' }}
                                         </span>
                                     </div>
                                 </li>
