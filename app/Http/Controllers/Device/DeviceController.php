232eb30f5a7123c93292479356fc520a0d3bb5ad
Huser Gaétan <gaetan.huser@netplus.ch>
2024-07-04 09:34:40 +0200

    fix missing class

--- app/Http/Controllers/Device/DeviceController.php
+++ app/Http/Controllers/Device/DeviceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Device;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Device;
 
 class DeviceController extends Controller
 {
